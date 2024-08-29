46791f3990360b04450ca3505f2c8325434cc581
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-08-29 15:48:19 +0300

    refactor: VS clean up solution

--- DiskSpaceAnalyzerMauiApp/MauiProgram.cs
+++ DiskSpaceAnalyzerMauiApp/MauiProgram.cs
@@ -16,7 +16,7 @@ namespace DiskSpaceAnalyzerMauiApp
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
