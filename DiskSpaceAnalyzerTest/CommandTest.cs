2f5bdeef553e277ff7f064472d51d5ea966426a9
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-08-30 16:44:49 +0300

    test: add command test

---
+++ DiskSpaceAnalyzerTest/CommandTest.cs
@@ -0,0 +1,24 @@
+﻿using DiskSpaceAnalyzerConsole.Models;
+using static DiskSpaceAnalyzerConsole.Constants;
+
+namespace DiskSpaceAnalyzerTest
+{
+    public class CommandTest
+    {
+        [Fact]
+        public void TestGetCommand()
+        {
+            Assert.Equal(Commands.Help, Command.GetCommand("help"));
+            Assert.Equal(Commands.Help, Command.GetCommand("h"));
+            Assert.Equal(Commands.Help, Command.GetCommand("HeLp"));
+            Assert.Equal(Commands.Help, Command.GetCommand("H"));
+
+            Assert.Equal(Commands.AnalyzedDirs, Command.GetCommand("analyzed_dirs"));
+            Assert.Equal(Commands.AnalyzedDirs, Command.GetCommand("ad"));
+            Assert.Equal(Commands.AnalyzedDirs, Command.GetCommand("AnAlYzEd_DiRs"));
+            Assert.Equal(Commands.AnalyzedDirs, Command.GetCommand("AD"));
+
+            Assert.Null(Command.GetCommand("dasdasd_dsa"));
+        }
+    }
+}
