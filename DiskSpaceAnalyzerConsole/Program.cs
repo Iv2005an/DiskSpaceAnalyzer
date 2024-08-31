72722b20bbcc9080019c2b00bd1973130618417b
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-08-31 15:49:07 +0300

    feat: add show available categories

--- DiskSpaceAnalyzerConsole/Program.cs
+++ DiskSpaceAnalyzerConsole/Program.cs
@@ -2,6 +2,7 @@
 using DiskSpaceAnalyzerConsole.Services;
 using DiskSpaceAnalyzerLib.Services;
 using static DiskSpaceAnalyzerConsole.Constants;
+using static DiskSpaceAnalyzerLib.Constants;
 
 Command? command = ArgsService.Parse(args);
 if (command is null) return;
@@ -62,5 +63,24 @@ switch (command.CommandName)
             Console.WriteLine(analyzed_dirs);
         }
         else PrintService.PrintWarningMessage("Analyzed directories are missing, run `analyze` command");
+        break;
+    case Commands.Categories:
+        PrintService.PrintSuccessMessage("Available categories for analysis:\n");
+        PrintService.PrintCategoryInfo("Raster", RasterExtensions);
+        PrintService.PrintCategoryInfo("Vector", VectorExtensions);
+        PrintService.PrintCategoryInfo("Text", TextExtensions);
+        PrintService.PrintCategoryInfo("Audio", AudioExtensions);
+        PrintService.PrintCategoryInfo("Video", VideoExtensions);
+        PrintService.PrintCategoryInfo("EBook", EBookExtensions);
+        PrintService.PrintCategoryInfo("CAD", CadExtensions);
+        PrintService.PrintCategoryInfo("Presentation", PresentationExtensions);
+        PrintService.PrintCategoryInfo("Spreadsheet", SpreadsheetExtensions);
+        PrintService.PrintCategoryInfo("Database", DatabaseExtensions);
+        PrintService.PrintCategoryInfo("Archive", ArchiveExtensions);
+        PrintService.PrintCategoryInfo("Web", WebExtensions);
+        PrintService.PrintCategoryInfo("Developer", DeveloperExtensions);
+        PrintService.PrintCategoryInfo("System", SystemExtensions);
+        PrintService.PrintCategoryInfo("Executables", ExecutablesExtensions);
+        PrintService.PrintCategoryInfo("Settings", SettingsExtensions);
         break;
 }
--- DiskSpaceAnalyzerConsole/Services/PrintService.cs
+++ DiskSpaceAnalyzerConsole/Services/PrintService.cs
@@ -13,5 +13,18 @@
         public static void PrintSuccessMessage(string message) => PrintMessage(message, ConsoleColor.Green);
         public static void PrintWarningMessage(string message) => PrintMessage(message, ConsoleColor.Yellow);
         public static void PrintErrorMessage(string message) => PrintMessage(message, ConsoleColor.Red);
+
+        public static void PrintCategoryInfo(string name, string[] extensions)
+        {
+            PrintInfoMessage($"\n{name}:");
+            string s = "";
+            for (int i = 0; i < extensions.Length; i++)
+            {
+                string extension = extensions[i];
+                s += $"{(i % 10 == 0 ? "\n " : ' ')}{extension}";
+            }
+
+            PrintWarningMessage($"{s}\n");
+        }
     }
 }
