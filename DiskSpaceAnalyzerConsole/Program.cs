42fc96b39be02875b53684c4b8e5ab561853a436
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-09-02 23:04:07 +0300

    fix: update print categories info

--- DiskSpaceAnalyzerConsole/Program.cs
+++ DiskSpaceAnalyzerConsole/Program.cs
@@ -2,7 +2,6 @@
 using DiskSpaceAnalyzerConsole.Services;
 using DiskSpaceAnalyzerLib.Services;
 using static DiskSpaceAnalyzerConsole.Constants;
-using static DiskSpaceAnalyzerLib.Constants;
 
 Command? command = ArgsService.Parse(args);
 if (command is null) return;
@@ -11,7 +10,6 @@ switch (command.CommandName)
 {
     case Commands.Help:
         PrintService.PrintCommandsDescriptions();
-
         break;
     case Commands.AnalyzedDirs:
         List<string> directoriesPath = await AnalyzedDirectoriesService.GetDirectoriesPaths();
@@ -19,29 +17,12 @@ switch (command.CommandName)
         {
             PrintService.PrintSuccessMessage("Analyzed directories:");
             string analyzed_dirs = "";
-            foreach (string path in directoriesPath)
-                analyzed_dirs += $"\n - {path}";
+            foreach (string path in directoriesPath) analyzed_dirs += $"\n - {path}";
             PrintService.PrintWarningMessage(analyzed_dirs);
         }
         else PrintService.PrintWarningMessage("Analyzed directories are missing, run `analyze` command");
         break;
     case Commands.Categories:
-        PrintService.PrintSuccessMessage("Available categories for analysis:\n");
-        PrintService.PrintCategoryInfo("Raster", RasterExtensions);
-        PrintService.PrintCategoryInfo("Vector", VectorExtensions);
-        PrintService.PrintCategoryInfo("Text", TextExtensions);
-        PrintService.PrintCategoryInfo("Audio", AudioExtensions);
-        PrintService.PrintCategoryInfo("Video", VideoExtensions);
-        PrintService.PrintCategoryInfo("EBook", EBookExtensions);
-        PrintService.PrintCategoryInfo("CAD", CadExtensions);
-        PrintService.PrintCategoryInfo("Presentation", PresentationExtensions);
-        PrintService.PrintCategoryInfo("Spreadsheet", SpreadsheetExtensions);
-        PrintService.PrintCategoryInfo("Database", DatabaseExtensions);
-        PrintService.PrintCategoryInfo("Archive", ArchiveExtensions);
-        PrintService.PrintCategoryInfo("Web", WebExtensions);
-        PrintService.PrintCategoryInfo("Developer", DeveloperExtensions);
-        PrintService.PrintCategoryInfo("System", SystemExtensions);
-        PrintService.PrintCategoryInfo("Executables", ExecutablesExtensions);
-        PrintService.PrintCategoryInfo("Settings", SettingsExtensions);
+        PrintService.PrintCategoriesInfo();
         break;
 }
