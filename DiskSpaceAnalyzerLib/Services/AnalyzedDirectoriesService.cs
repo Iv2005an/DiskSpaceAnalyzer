47e3b63c2907b520877aac59f4abf80ef11f45c3
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-09-10 08:41:19 +0300

    feat: analyze method

--- DiskSpaceAnalyzerLib/Services/AnalyzedDirectoriesService.cs
+++ DiskSpaceAnalyzerLib/Services/AnalyzedDirectoriesService.cs
@@ -1,9 +1,34 @@
 ﻿using DiskSpaceAnalyzerLib.Databases;
+using DiskSpaceAnalyzerLib.Models;
 
 namespace DiskSpaceAnalyzerLib.Services;
 
 public static class AnalyzedDirectoriesService
 {
+    public static async Task Analyze(string path, bool isRepeat, IProgress<string>? progress = null)
+    {
+        if (isRepeat) await DeleteDirectoryAsync(path);
+        if (isRepeat || !await IsAnalyzedDirectoryAsync(path))
+        {
+            try
+            {
+                List<Task> tasks = [];
+                foreach (string directory in Directory.GetDirectories(path))
+                    await Analyze(directory, isRepeat, progress);
+                await AnalyzedFilesDatabase.DeleteFilesAsync(file => file.DirectoryPath == path);
+                foreach (string file in Directory.GetFiles(path))
+                    tasks.Add(AnalyzedFilesDatabase.AddFileAsync(new AnalyzedFile(file)));
+                await Task.WhenAll(tasks);
+                await AnalyzedDirectoriesDatabase.AddDirectoryAsync(new AnalyzedDirectory(path));
+            }
+            catch (UnauthorizedAccessException) { }
+            catch (Exception)
+            {
+                await AnalyzedFilesDatabase.AddFileAsync(new AnalyzedFile(path, true));
+            }
+            progress?.Report(path);
+        }
+    }
     public static async Task<List<string>> GetDirectoriesPaths() =>
        (await AnalyzedDirectoriesDatabase.GetDirectoriesAsync()).Select(
            directory => directory.DirectoryPath).ToList();
