e4e1d3f677d2d2bff64fb7a63107f14e5855a0bb
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2025-03-10 21:15:04 +0300

    feat: add check to a child folder

---
+++ DiskSpaceAnalyzerLib/Extensions/DirectoryInfoExtension.cs
@@ -0,0 +1,14 @@
+namespace DiskSpaceAnalyzerLib.Extensions;
+
+public static class DirectoryInfoExtension
+{
+    public static bool IsChildDirectoryOf(this DirectoryInfo childDir, DirectoryInfo parentDir)
+    {
+        while (childDir.Parent != null)
+        {
+            if (childDir.Parent.FullName == parentDir.FullName) return true;
+            else childDir = childDir.Parent;
+        }
+        return false;
+    }
+}
---
+++ DiskSpaceAnalyzerTest/LibTests/DirectoryInfoExtensionsTest.cs
@@ -0,0 +1,23 @@
+using DiskSpaceAnalyzerLib.Extensions;
+namespace DiskSpaceAnalyzerTest.LibTests
+
+{
+    public class DirectoryInfoExtensionsTests
+    {
+        [Theory]
+        [InlineData(@"C:\Parent", @"C:\Parent\Child", true)] // Windows
+        [InlineData(@"C:\Parent", @"C:\Parent\Child\SubChild", true)] // Windows
+        [InlineData(@"C:\Parent", @"C:\Other\Child", false)] // Windows
+        [InlineData("/home/user/Parent", "/home/user/Parent/Child", true)] // Linux
+        [InlineData("/home/user/Parent", "/home/user/Other/Child", false)] // Linux
+        [InlineData("/Users/user/Parent", "/Users/user/Parent/Child", true)] // macOS
+        [InlineData("/Users/user/Parent", "/Users/user/Other/Child", false)] // macOS
+        public void IsChildDirectoryOfTest(string parentPath, string childPath, bool expectedResult)
+        {
+            var parentDir = new DirectoryInfo(parentPath);
+            var childDir = new DirectoryInfo(childPath);
+
+            Assert.Equal(expectedResult, childDir.IsChildDirectoryOf(parentDir));
+        }
+    }
+}
