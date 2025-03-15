b82076dca17d89e0882a01ba848ab626be7c4c3a
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2025-03-15 19:13:09 +0300

    feat: add analyze tests

--- DiskSpaceAnalyzerTest/LibTests/DirectoryServiceTest.cs
+++ DiskSpaceAnalyzerTest/LibTests/DirectoryServiceTest.cs
@@ -1,37 +1,52 @@
 using DiskSpaceAnalyzerLib.Databases;
+using DiskSpaceAnalyzerLib.Models;
 using DiskSpaceAnalyzerLib.Services;
 
-namespace DiskSpaceAnalyzerTest.LibTests
+namespace DiskSpaceAnalyzerTest.LibTests;
 
+public class DirectoryServiceTest
 {
-    public class DirectoryServiceTest
+    [Fact]
+    public async Task SimpleAnalyzeTest()
     {
-        static DirectoryInfo GetSolutionPath()
-        {
-            DirectoryInfo directory = new(Environment.CurrentDirectory);
-            while (directory.Parent is not null)
-            {
-                if (directory.GetFiles("*.sln").Length != 0) return directory;
-                directory = directory.Parent;
-            }
-            return new(Environment.CurrentDirectory);
-        }
+        DirectoryInfo[] dirs = Helper.Prepare("SimpleAnalyzeTest");
+        await DirectoryService.Analyze([dirs[0]]);
 
-        [Fact]
-        public async Task AnalyzeTest()
-        {
-            DirectoryInfo sourceDir = new(Path.Combine(GetSolutionPath().FullName, "TestDir"));
-            DirectoryInfo outputDir = new(Path.Combine(GetSolutionPath().FullName, "OutputTestDir"));
-            DirectoryInfo dbsDir = new(Path.Combine(outputDir.FullName, "dbs"));
-            dbsDir.Create();
+        List<AnalyzedDirectory> directories = await DirectoryDatabase.GetDirectoriesAsync();
+        int fileCount = await FileDatabase.GetFilesCountAsync();
+        int directoryCount = directories.Count;
+        int dirFilesCount = directories.Select(x => x.FileCount).Sum();
 
-            Database.DatabasePath = Path.Combine(outputDir.FullName, "dbs");
-            Database.DatabaseFilename = "1.db3";
-            await DirectoryService.Analyze([sourceDir]);
+        Assert.True(fileCount == dirFilesCount);
+        Assert.Equal(700, fileCount);
+        Assert.Equal(15, directoryCount);
+    }
 
-            Database.DatabasePath = Path.Combine(outputDir.FullName, "dbs");
-            Database.DatabaseFilename = "2.db3";
-            await DirectoryService.Analyze([sourceDir], [new(Path.Combine(sourceDir.FullName, "Смешарики. Выпуск 1"))]);
-        }
+    [Fact]
+    public async Task IgnoreAnalyzeTest()
+    {
+        DirectoryInfo[] dirs = Helper.Prepare("IgnoreAnalyzeTest");
+        await DirectoryService.Analyze([dirs[0]], [new(Path.Combine(dirs[0].FullName, "Фото 1"))]);
+
+        int fileCount = await FileDatabase.GetFilesCountAsync();
+        int directoryCount = await DirectoryDatabase.GetDirectoriesCountAsync();
+        Assert.Equal(296, fileCount);
+        Assert.Equal(10, directoryCount);
+    }
+
+    [Fact]
+    public async Task MultipleIgnoreAnalyzeTest()
+    {
+        DirectoryInfo[] dirs = Helper.Prepare("MultipleIgnoreAnalyzeTest");
+        string sourceDir = dirs[0].FullName;
+        await DirectoryService.Analyze([dirs[0]], [
+            new(Path.Combine(sourceDir, "Фото 1")),
+            new(Path.Combine(sourceDir, "Фото 2")),
+        ]);
+
+        int fileCount = await FileDatabase.GetFilesCountAsync();
+        int directoryCount = await DirectoryDatabase.GetDirectoriesCountAsync();
+        Assert.Equal(195, fileCount);
+        Assert.Equal(9, directoryCount);
     }
 }
