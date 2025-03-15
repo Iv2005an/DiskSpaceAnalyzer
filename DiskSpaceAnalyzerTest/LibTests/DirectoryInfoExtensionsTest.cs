c2eae03aed74e721d319f0aa4f396a62ddc55e5c
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2025-03-16 00:38:42 +0300

    test: add test for DirectoryInfoExtensions

--- DiskSpaceAnalyzerTest/LibTests/DirectoryInfoExtensionsTest.cs
+++ DiskSpaceAnalyzerTest/LibTests/DirectoryInfoExtensionsTest.cs
@@ -14,9 +14,33 @@ public class DirectoryInfoExtensionsTests
     [InlineData("/Users/user/Parent", "/Users/user/Other/Child", false)] // macOS
     public void IsChildDirectoryOfTest(string parentPath, string childPath, bool expectedResult)
     {
-        var parentDir = new DirectoryInfo(parentPath);
-        var childDir = new DirectoryInfo(childPath);
+        DirectoryInfo parentDir = new(parentPath);
+        DirectoryInfo childDir = new(childPath);
 
         Assert.Equal(expectedResult, childDir.IsChildDirectoryOf(parentDir));
+    }
+
+    [Theory]
+    [InlineData(@"C:\Test", @"C:\Test\Child", true)] // Windows прямой потомок
+    [InlineData(@"C:\Test", @"C:\Test\Child\Grandchild", true)] // Windows косвенный потомок
+    [InlineData("/home/user/test", "/home/user/test/subdir", true)] // Linux прямой потомок
+    [InlineData("/home/user/test", "/home/user/test/subdir/grandchild", true)] // Linux косвенный потомок
+    [InlineData("/Users/user/test", "/Users/user/test/subdir", true)] // macOS прямой потомок
+    [InlineData("/Users/user/test", "/Users/user/test/subdir/grandchild", true)] // macOS косвенный потомок
+    [InlineData(@"C:\Test", @"C:\Test", true)] // Windows сама директория
+    [InlineData("/home/user/test", "/home/user/test", true)] // Linux сама директория
+    [InlineData("/Users/user/test", "/Users/user/test", true)] // macOS сама директория
+    [InlineData(@"C:\Test", @"D:\Unrelated", false)] // Windows несвязанная директория
+    [InlineData("/home/user/test", "/var/log", false)] // Linux несвязанная директория
+    [InlineData("/Users/user/test", "/Library/Frameworks", false)] // macOS несвязанная директория
+    [InlineData(@"C:\Test", null, false)] // Windows null-сценарий
+    [InlineData("/home/user/test", null, false)] // Linux null-сценарий
+    [InlineData("/Users/user/test", null, false)] // macOS null-сценарий
+    public void IsParentDirectoryOfAnyTest(string parentPath, string childPath, bool expected)
+    {
+        var parentDir = new DirectoryInfo(parentPath);
+        List<DirectoryInfo>? dirs = childPath == null ? null : [new(childPath)];
+
+        Assert.Equal(expected, parentDir.IsParentDirectoryOfAny(dirs));
     }
 }
