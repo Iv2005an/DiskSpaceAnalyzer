584021ba8b3c37fabc594e8dc9c0405cf2fa08f8
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2025-03-15 18:53:07 +0300

    test, refactor: remove namespace curly braces

--- DiskSpaceAnalyzerTest/LibTests/DirectoryInfoExtensionsTest.cs
+++ DiskSpaceAnalyzerTest/LibTests/DirectoryInfoExtensionsTest.cs
@@ -1,23 +1,22 @@
 using DiskSpaceAnalyzerLib.Extensions;
-namespace DiskSpaceAnalyzerTest.LibTests
+namespace DiskSpaceAnalyzerTest.LibTests;
 
+
+public class DirectoryInfoExtensionsTests
 {
-    public class DirectoryInfoExtensionsTests
+    [Theory]
+    [InlineData(@"C:\Parent", @"C:\Parent\Child", true)] // Windows
+    [InlineData(@"C:\Parent", @"C:\Parent\Child\SubChild", true)] // Windows
+    [InlineData(@"C:\Parent", @"C:\Other\Child", false)] // Windows
+    [InlineData("/home/user/Parent", "/home/user/Parent/Child", true)] // Linux
+    [InlineData("/home/user/Parent", "/home/user/Other/Child", false)] // Linux
+    [InlineData("/Users/user/Parent", "/Users/user/Parent/Child", true)] // macOS
+    [InlineData("/Users/user/Parent", "/Users/user/Other/Child", false)] // macOS
+    public void IsChildDirectoryOfTest(string parentPath, string childPath, bool expectedResult)
     {
-        [Theory]
-        [InlineData(@"C:\Parent", @"C:\Parent\Child", true)] // Windows
-        [InlineData(@"C:\Parent", @"C:\Parent\Child\SubChild", true)] // Windows
-        [InlineData(@"C:\Parent", @"C:\Other\Child", false)] // Windows
-        [InlineData("/home/user/Parent", "/home/user/Parent/Child", true)] // Linux
-        [InlineData("/home/user/Parent", "/home/user/Other/Child", false)] // Linux
-        [InlineData("/Users/user/Parent", "/Users/user/Parent/Child", true)] // macOS
-        [InlineData("/Users/user/Parent", "/Users/user/Other/Child", false)] // macOS
-        public void IsChildDirectoryOfTest(string parentPath, string childPath, bool expectedResult)
-        {
-            var parentDir = new DirectoryInfo(parentPath);
-            var childDir = new DirectoryInfo(childPath);
+        var parentDir = new DirectoryInfo(parentPath);
+        var childDir = new DirectoryInfo(childPath);
 
-            Assert.Equal(expectedResult, childDir.IsChildDirectoryOf(parentDir));
-        }
+        Assert.Equal(expectedResult, childDir.IsChildDirectoryOf(parentDir));
     }
 }
