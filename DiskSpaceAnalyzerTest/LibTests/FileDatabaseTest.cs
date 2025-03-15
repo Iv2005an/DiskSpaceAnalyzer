1bf7562087c1a5d221ceec5cc4ce11d5fb9ca131
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2025-03-15 18:51:21 +0300

    test: add test for FileDatabase

---
+++ DiskSpaceAnalyzerTest/LibTests/FileDatabaseTest.cs
@@ -0,0 +1,41 @@
+using DiskSpaceAnalyzerLib.Databases;
+using DiskSpaceAnalyzerLib.Models;
+using DiskSpaceAnalyzerLib.Services;
+
+namespace DiskSpaceAnalyzerTest.LibTests;
+
+public class FileDatabaseTest
+{
+    [Fact]
+    public async Task AddFileTest()
+    {
+        Helper.Prepare("AddFileTest");
+        await FileDatabase.AddFileAsync(new());
+        Assert.Equal(1, await FileDatabase.GetFilesCountAsync());
+    }
+
+    [Fact]
+    public async Task DeleteFilesTest()
+    {
+        Helper.Prepare("DeleteFilesTest");
+        await FileDatabase.AddFileAsync(new());
+        Assert.Equal(1, await FileDatabase.GetFilesCountAsync());
+        await FileDatabase.DeleteFilesAsync(dir => true);
+        Assert.Equal(0, await FileDatabase.GetFilesCountAsync());
+    }
+
+    [Fact]
+    public async Task FileCountTest()
+    {
+        Helper.Prepare("FileCountTest");
+
+        for (int i = 0; i < 5; i++)
+        {
+            await FileDatabase.AddFileAsync(new());
+        }
+        List<AnalyzedFile> directories = await FileDatabase.GetFilesAsync();
+
+        Assert.Equal(5, directories.Count);
+        Assert.Equal(5, await FileDatabase.GetFilesCountAsync());
+    }
+}
