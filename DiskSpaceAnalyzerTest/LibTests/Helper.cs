21c4e573645b4b21d4e47ca2655e484608b226d4
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2025-03-15 18:50:41 +0300

    test: add helper class

---
+++ DiskSpaceAnalyzerTest/LibTests/Helper.cs
@@ -0,0 +1,33 @@
+using DiskSpaceAnalyzerLib.Databases;
+
+namespace DiskSpaceAnalyzerTest.LibTests;
+
+public class Helper
+{
+    public static DirectoryInfo GetSolutionPath()
+    {
+        DirectoryInfo directory = new(Environment.CurrentDirectory);
+        while (directory.Parent is not null)
+        {
+            if (directory.GetFiles("*.sln").Length != 0) return directory;
+            directory = directory.Parent;
+        }
+        return new(Environment.CurrentDirectory);
+    }
+
+    public static DirectoryInfo[] Prepare(string testName)
+    {
+        string solutionPath = GetSolutionPath().FullName;
+        DirectoryInfo sourceDir = new(Path.Combine(solutionPath, "TestDir"));
+        DirectoryInfo outputDir = new(Path.Combine(solutionPath, "OutputTestDir", testName));
+        DirectoryInfo databasesDir = new(Path.Combine(outputDir.FullName, "_databases"));
+
+        outputDir.Create();
+        outputDir.Delete(true);
+        databasesDir.Create();
+
+        Database.DatabasePath = databasesDir.FullName;
+
+        return [sourceDir, outputDir, databasesDir];
+    }
+}
