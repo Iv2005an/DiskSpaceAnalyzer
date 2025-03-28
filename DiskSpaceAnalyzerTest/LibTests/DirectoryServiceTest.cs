b5a18c05f37475b8fc29f986bb2ab5e817e65761
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2025-03-28 05:06:04 +0300

    test: update tests

--- DiskSpaceAnalyzerTest/LibTests/DirectoryServiceTest.cs
+++ DiskSpaceAnalyzerTest/LibTests/DirectoryServiceTest.cs
@@ -19,7 +19,7 @@ public class DirectoryServiceTest
         int dirFilesCount = directories.Select(x => x.FileCount).Sum();
 
         Assert.True(fileCount == dirFilesCount);
-        Assert.Equal(61, fileCount);
+        Assert.Equal(51, fileCount);
         Assert.Equal(19, directoryCount);
     }
 
@@ -35,7 +35,7 @@ public class DirectoryServiceTest
         int dirFilesCount = directories.Select(x => x.FileCount).Sum();
 
         Assert.True(fileCount == dirFilesCount);
-        Assert.Equal(61, fileCount);
+        Assert.Equal(51, fileCount);
         Assert.Equal(19, directoryCount);
     }
 
@@ -47,7 +47,7 @@ public class DirectoryServiceTest
 
         int fileCount = await FileDatabase.GetFilesCountAsync();
         int directoryCount = await DirectoryDatabase.GetDirectoriesCountAsync();
-        Assert.Equal(50, fileCount);
+        Assert.Equal(41, fileCount);
         Assert.Equal(17, directoryCount);
     }
 
@@ -59,7 +59,7 @@ public class DirectoryServiceTest
 
         int fileCount = await FileDatabase.GetFilesCountAsync();
         int directoryCount = await DirectoryDatabase.GetDirectoriesCountAsync();
-        Assert.Equal(50, fileCount);
+        Assert.Equal(41, fileCount);
         Assert.Equal(17, directoryCount);
     }
 
@@ -76,7 +76,7 @@ public class DirectoryServiceTest
 
         int fileCount = await FileDatabase.GetFilesCountAsync();
         int directoryCount = await DirectoryDatabase.GetDirectoriesCountAsync();
-        Assert.Equal(49, fileCount);
+        Assert.Equal(40, fileCount);
         Assert.Equal(15, directoryCount);
     }
 
@@ -89,92 +89,74 @@ public class DirectoryServiceTest
         CategoryInfo raster = categories[(int)Categories.Raster];
         Assert.Empty(raster.Files);
         Assert.Equal(5, raster.Duplicates.Count);
-        Assert.Equal((float)0.2, raster.Percentages);
 
         CategoryInfo vector = categories[(int)Categories.Vector];
         Assert.Empty(vector.Files);
         Assert.Empty(vector.Duplicates);
-        Assert.Equal(0, vector.Percentages);
 
         CategoryInfo text = categories[(int)Categories.Text];
         Assert.Single(text.Files);
         Assert.Equal(3, text.Duplicates.Count);
-        Assert.Equal((float)0.16, text.Percentages);
 
         CategoryInfo audio = categories[(int)Categories.Audio];
         Assert.Empty(audio.Files);
         Assert.Equal(5, audio.Duplicates.Count);
-        Assert.Equal((float)0.2, audio.Percentages);
 
         CategoryInfo video = categories[(int)Categories.Video];
         Assert.Empty(video.Files);
         Assert.Empty(video.Duplicates);
-        Assert.Equal(0, video.Percentages);
 
         CategoryInfo eBook = categories[(int)Categories.EBook];
         Assert.Empty(eBook.Files);
         Assert.Empty(eBook.Duplicates);
-        Assert.Equal(0, eBook.Percentages);
 
         CategoryInfo cad = categories[(int)Categories.CAD];
         Assert.Empty(cad.Files);
         Assert.Empty(cad.Duplicates);
-        Assert.Equal(0, cad.Percentages);
 
         CategoryInfo presentation = categories[(int)Categories.Presentation];
         Assert.Empty(presentation.Files);
         Assert.Empty(presentation.Duplicates);
-        Assert.Equal(0, presentation.Percentages);
 
         CategoryInfo spreadsheet = categories[(int)Categories.Spreadsheet];
         Assert.Empty(spreadsheet.Files);
         Assert.Empty(spreadsheet.Duplicates);
-        Assert.Equal(0, spreadsheet.Percentages);
 
         CategoryInfo database = categories[(int)Categories.Database];
         Assert.Empty(database.Files);
         Assert.Empty(database.Duplicates);
-        Assert.Equal(0, database.Percentages);
 
         CategoryInfo archive = categories[(int)Categories.Archive];
         Assert.Empty(archive.Files);
         Assert.Single(archive.Duplicates);
-        Assert.Equal((float)0.04, archive.Percentages);
 
         CategoryInfo web = categories[(int)Categories.Web];
         Assert.Empty(web.Files);
         Assert.Equal(3, web.Duplicates.Count);
-        Assert.Equal((float)0.12, web.Percentages);
 
         CategoryInfo developer = categories[(int)Categories.Developer];
         Assert.Empty(developer.Files);
         Assert.Empty(developer.Duplicates);
-        Assert.Equal(0, developer.Percentages);
 
         CategoryInfo system = categories[(int)Categories.System];
         Assert.Empty(system.Files);
         Assert.Equal(2, system.Duplicates.Count);
-        Assert.Equal((float)0.08, system.Percentages);
 
         CategoryInfo executables = categories[(int)Categories.Executables];
         Assert.Empty(executables.Files);
         Assert.Empty(executables.Duplicates);
-        Assert.Equal(0, executables.Percentages);
 
         CategoryInfo settings = categories[(int)Categories.Settings];
         Assert.Empty(settings.Files);
         Assert.Empty(settings.Duplicates);
-        Assert.Equal(0, settings.Percentages);
 
         CategoryInfo other = categories[(int)Categories.Other];
-        Assert.Equal(3, other.Files.Count);
-        Assert.Equal(2, other.Duplicates.Count);
-        Assert.Equal((float)0.2, other.Percentages);
+        Assert.Empty(other.Files);
+        Assert.Single(other.Duplicates);
 
         CategoryInfo error = categories[(int)Categories.Error];
         Assert.Empty(error.Files);
         Assert.Empty(error.Duplicates);
-        Assert.Equal(0, error.Percentages);
     }
 
     [Fact]
@@ -188,92 +170,74 @@ public class DirectoryServiceTest
         CategoryInfo raster = categories[(int)Categories.Raster];
         Assert.Equal(12, raster.Files.Count);
         Assert.Single(raster.Duplicates);
-        Assert.Equal((float)0.3171, raster.Percentages);
 
         CategoryInfo vector = categories[(int)Categories.Vector];
         Assert.Empty(vector.Files);
         Assert.Empty(vector.Duplicates);
-        Assert.Equal(0, vector.Percentages);
 
         CategoryInfo text = categories[(int)Categories.Text];
         Assert.Single(text.Files);
         Assert.Equal(3, text.Duplicates.Count);
-        Assert.Equal((float)0.0976, text.Percentages);
 
         CategoryInfo audio = categories[(int)Categories.Audio];
         Assert.Equal(12, audio.Files.Count);
         Assert.Single(audio.Duplicates);
-        Assert.Equal((float)0.3171, audio.Percentages);
 
         CategoryInfo video = categories[(int)Categories.Video];
         Assert.Empty(video.Files);
         Assert.Empty(video.Duplicates);
-        Assert.Equal(0, video.Percentages);
 
         CategoryInfo eBook = categories[(int)Categories.EBook];
         Assert.Empty(eBook.Files);
         Assert.Empty(eBook.Duplicates);
-        Assert.Equal(0, eBook.Percentages);
 
         CategoryInfo cad = categories[(int)Categories.CAD];
         Assert.Empty(cad.Files);
         Assert.Empty(cad.Duplicates);
-        Assert.Equal(0, cad.Percentages);
 
         CategoryInfo presentation = categories[(int)Categories.Presentation];
         Assert.Empty(presentation.Files);
         Assert.Empty(presentation.Duplicates);
-        Assert.Equal(0, presentation.Percentages);
 
         CategoryInfo spreadsheet = categories[(int)Categories.Spreadsheet];
         Assert.Empty(spreadsheet.Files);
         Assert.Empty(spreadsheet.Duplicates);
-        Assert.Equal(0, spreadsheet.Percentages);
 
         CategoryInfo database = categories[(int)Categories.Database];
         Assert.Empty(database.Files);
         Assert.Empty(database.Duplicates);
-        Assert.Equal(0, database.Percentages);
 
         CategoryInfo archive = categories[(int)Categories.Archive];
         Assert.Empty(archive.Files);
         Assert.Single(archive.Duplicates);
-        Assert.Equal((float)0.0244, archive.Percentages);
 
         CategoryInfo web = categories[(int)Categories.Web];
         Assert.Empty(web.Files);
         Assert.Equal(3, web.Duplicates.Count);
-        Assert.Equal((float)0.0732, web.Percentages);
 
         CategoryInfo developer = categories[(int)Categories.Developer];
         Assert.Empty(developer.Files);
         Assert.Empty(developer.Duplicates);
-        Assert.Equal(0, developer.Percentages);
 
         CategoryInfo system = categories[(int)Categories.System];
         Assert.Empty(system.Files);
         Assert.Equal(2, system.Duplicates.Count);
-        Assert.Equal((float)0.0488, system.Percentages);
 
         CategoryInfo executables = categories[(int)Categories.Executables];
         Assert.Empty(executables.Files);
         Assert.Empty(executables.Duplicates);
-        Assert.Equal(0, executables.Percentages);
 
         CategoryInfo settings = categories[(int)Categories.Settings];
         Assert.Empty(settings.Files);
         Assert.Empty(settings.Duplicates);
-        Assert.Equal(0, settings.Percentages);
 
         CategoryInfo other = categories[(int)Categories.Other];
-        Assert.Equal(3, other.Files.Count);
-        Assert.Equal(2, other.Duplicates.Count);
-        Assert.Equal((float)0.122, other.Percentages);
+        Assert.Empty(other.Files);
+        Assert.Single(other.Duplicates);
 
         CategoryInfo error = categories[(int)Categories.Error];
         Assert.Empty(error.Files);
         Assert.Empty(error.Duplicates);
-        Assert.Equal(0, error.Percentages);
     }
 
     [Fact]
@@ -297,6 +261,6 @@ public class DirectoryServiceTest
     {
         DirectoryInfo[] dirs = Helper.Prepare("CategoryFilterOrganizeTest");
         await DirectoryService.Analyze([dirs[0]]);
-        await DirectoryService.Organize(dirs[1], [dirs[0]], categories:[Categories.Raster, Categories.Archive]);
+        await DirectoryService.Organize(dirs[1], [dirs[0]], categories: [Categories.Raster, Categories.Archive]);
     }
 }
