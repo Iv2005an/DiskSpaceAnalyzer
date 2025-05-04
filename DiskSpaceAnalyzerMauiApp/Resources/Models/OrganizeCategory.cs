2a8469aeddab8384e98e8c2e8f48b072a5b44e95
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2025-05-04 22:31:20 +0300

    feat: uncheck all categories by default

--- DiskSpaceAnalyzerMauiApp/Resources/Models/OrganizeCategory.cs
+++ DiskSpaceAnalyzerMauiApp/Resources/Models/OrganizeCategory.cs
@@ -9,7 +9,7 @@ public partial class OrganizeCategory(CategoryInfo categoryInfo) : ObservableObj
 
     public event SelectedHandler? SelectedChanged;
     public CategoryInfo CategoryInfo { get; set; } = categoryInfo;
-    [ObservableProperty] public partial bool Selected { get; set; } = true;
+    [ObservableProperty] public partial bool Selected { get; set; }
     public Color Color { get; private init; } = CategoryData.GetColor(categoryInfo.Category);
     public string Name { get; private init; } = CategoryData.GetName(categoryInfo.Category);
     public int FileCount { get; private init; } = categoryInfo.Count;
